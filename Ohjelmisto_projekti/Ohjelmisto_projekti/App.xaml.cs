4a8651805d7db67654dac4e777f52b4788fb340e
Miro Kähärä <miro23005@student.hamk.fi>
2024-01-31 13:55:42 +0200

    Aloituskansioiden lisääminen

---
+++ Ohjelmisto_projekti/Ohjelmisto_projekti/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ohjelmisto_projekti
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
