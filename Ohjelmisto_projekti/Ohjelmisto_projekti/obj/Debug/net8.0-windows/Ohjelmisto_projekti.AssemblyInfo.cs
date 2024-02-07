e8f3def157b0e3cacecb8e1eb9140a0c97209de6
Turtlesaurus05 <joonatan.a.lipiainen@gmail.com>
2024-02-07 13:11:37 +0200

    työaika päivitys

--- Ohjelmisto_projekti/Ohjelmisto_projekti/obj/Debug/net8.0-windows/Ohjelmisto_projekti.AssemblyInfo.cs
+++ Ohjelmisto_projekti/Ohjelmisto_projekti/obj/Debug/net8.0-windows/Ohjelmisto_projekti.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ohjelmisto_projekti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1e0e01cc3c95d043510a00c6ec88fe65e7b1f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c36495f66395a103221ee3c4b9a48fdf9b6d3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ohjelmisto_projekti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ohjelmisto_projekti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
