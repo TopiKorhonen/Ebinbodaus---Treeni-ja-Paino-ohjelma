05c9c5fa561696abc7b4ca6b29894d60d65fd3b6
Topi <topiek00@gmail.com>
2024-03-02 05:26:06 +0200

    Sivujen yläpalkit poistettu

--- Ohjelmisto_projekti/Ohjelmisto_projekti/obj/Debug/net8.0-windows/Ohjelmisto_projekti.AssemblyInfo.cs
+++ Ohjelmisto_projekti/Ohjelmisto_projekti/obj/Debug/net8.0-windows/Ohjelmisto_projekti.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ohjelmisto_projekti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2390559acc17ce42bdc4f87f679f29bb7544672c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed92ce64e086be5af685f65022308cec0548a5f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ohjelmisto_projekti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ohjelmisto_projekti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
