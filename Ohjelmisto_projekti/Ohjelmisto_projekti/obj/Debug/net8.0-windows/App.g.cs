4e6234f4b6a15e2e26d2ccfbddbe4cfad097db92
John Ramstedt <john22002@student.hamk.fi>
2024-03-09 18:29:15 +0200

    työaika päivitetty

--- Ohjelmisto_projekti/Ohjelmisto_projekti/obj/Debug/net8.0-windows/App.g.cs
+++ Ohjelmisto_projekti/Ohjelmisto_projekti/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace Ohjelmisto_projekti {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Ohjelmisto_projekti {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
         public static void Main() {
             Ohjelmisto_projekti.App app = new Ohjelmisto_projekti.App();
             app.InitializeComponent();
